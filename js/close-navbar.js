eb7bf455155b35b8c591c3415e17b8ffbd8eac03
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2018-05-05 20:18:45 -0400

    Add sub-menus capabilities and fix local files links

--- js/close-navbar.js
+++ js/close-navbar.js
@@ -1,6 +1,6 @@
 $(function () {
 	
-	$("a.nav-item,a.dropdown-item").click(function (e) {
+	$("a.nav-item,a.dropdown-item:not(.dropdown-toggle)").click(function (e) {
 		
 		$("#navbar-toggler[aria-expanded=true]").click();
 		
