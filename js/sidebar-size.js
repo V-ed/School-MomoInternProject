3b29fe549a179da8b979f61944220bf49d100ddc
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2018-05-08 18:06:32 -0400

    Optimize sidebar-size script

--- js/sidebar-size.js
+++ js/sidebar-size.js
@@ -8,21 +8,15 @@ $(function () {
 	var stickyHeaderHeight = $stickyHeader.outerHeight(true);
 	var stickyContainerHeight = $stickyContainer.outerHeight(true);
 	
-	var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-	
 	$stickies.each(function () {
 		
 		$(this).css({
 			"position": "sticky",
 			"top": stickyHeaderHeight + "px",
 			"height": "calc(100vh - " + stickyHeaderHeight + "px)",
-			"max-height": "calc(100vh - " + stickyHeaderHeight + "px)",
+			"max-height": stickyContainerHeight + "px",
 			"overflow-y": "auto"
 		});
-		
-		if (viewportHeight > stickyContainerHeight) {
-			$(this).css("height", stickyContainerHeight + "px");
-		}
 		
 	});
 	
