477173be15b00404afb4d59867f0aec3b64b1ddc
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2018-05-19 14:25:12 -0400

    Add git merge section's description to sync page & <pre> code formatting

--- js/loadings.js
+++ js/loadings.js
@@ -6,4 +6,10 @@ $(function () {
 	
 	$("[data-toggle='tooltip']").tooltip();
 	
+	$("pre.code").each(function () {
+        var html = $(this).html();
+        var blankLen = (html.split('\n')[0].match(/^[\s\t]+/)[0]).length;
+        $(this).html($.trim(html.replace(eval("/^[\s\t]{" + blankLen + "}/gm"), "")));
+    });
+	
 });
